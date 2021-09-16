dc8c42ce52a3965700cc2559ea5c0d266a857f00
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-16 20:48:24 +0530

    Added user registration use case 2: Last name

--- src/com/userregis/UserRegistration.java
+++ src/com/userregis/UserRegistration.java
@@ -6,14 +6,23 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		
+		//pattern
 		final String FIRST_NAME = "^[A-Z][a-z]{2}[a-z]*";
-
+		final String LAST_NAME = "^[A-Z][a-z]{2}[a-z]*";
+		
 		String firstName = "Anirudha";
+		String lastName = "Mayya";
 
-		if (Pattern.matches(FIRST_NAME, firstName)) { // USECASE1
+		if (Pattern.matches(FIRST_NAME, firstName)) { // use case 1 : first name
 			System.out.println("Valid first name");
 		} else {
 			System.out.println("Invalid");
+		}
+		
+		if (Pattern.matches(LAST_NAME, lastName)) { // use case 2 : last name
+			System.out.println("Valid last name");
+		} else {
+			System.out.println("Invalid last name");
 		}
 
 	}
